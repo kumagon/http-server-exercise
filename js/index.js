c7db5d1c43497c534bedb85675f2b2ca208e589d
kumagon <kumagon0619@gmail.com>
2022-10-15 14:41:05 +0900

    js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
-function clickHandler() {
-            const box = document.getElementById("box");
-            const p = document.createElement("p");
-            p.innerText = "Added by JS";
-            box.appendChild(p);
-            console.log('add JS');
-        }
+function clickHandlerJS() {
+    const box = document.getElementById("box");
+    const p = document.createElement("p");
+    p.innerText = "Added by JS";
+    box.appendChild(p);
+    console.log('add JS');
+}
+function clickHandlerPHP() {
+    const box = document.getElementById("box");
+    const p = document.createElement("p");
+    p.innerText = "Added by PHP";
+    box.appendChild(p);
+    console.log('add PHP');
+}
+function clickHandlerClear() {
+    const box = document.getElementById("box");
+    box.innerHTML = null;
+}
